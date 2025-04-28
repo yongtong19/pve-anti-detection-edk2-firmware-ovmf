b8aedcfc9b5726e0b7405c9bed4efcd4a595d4c2
Yongtong Liu <Liu@yongtong.me>
2025-04-28 16:57:35 -0400

    update logo

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-sudo sed -i 's|https://mirrors.ustc.edu.cn/proxmox|http://download.proxmox.com|g' /etc/apt/ceph.list
-sudo sed -i 's|https://mirrors.ustc.edu.cn/proxmox|http://download.proxmox.com|g' /etc/apt/pve-no-subscription.list
+sudo sed -i 's|https://mirrors.ustc.edu.cn/proxmox|http://download.proxmox.com|g' /etc/apt/sources.list.d/ceph.list
+sudo sed -i 's|https://mirrors.ustc.edu.cn/proxmox|http://download.proxmox.com|g' /etc/apt/sources.list.d/pve-no-subscription.list
 #以下两行docker里面运行需要sudo 如果pve里面运行请删除sudo
 sudo apt-get update
 sudo apt-get install -y pve-qemu-kvm gcc-aarch64-linux-gnu gcc-riscv64-linux-gnu libacl1-dev libaio-dev libattr1-dev libcap-ng-dev libcurl4-gnutls-dev libepoxy-dev libfdt-dev libgbm-dev libglusterfs-dev libgnutls28-dev libiscsi-dev libjpeg-dev libnuma-dev libpci-dev libpixman-1-dev libproxmox-backup-qemu0-dev librbd-dev libsdl1.2-dev libseccomp-dev libslirp-dev libspice-protocol-dev libspice-server-dev libsystemd-dev liburing-dev libusb-1.0-0-dev libusbredirparser-dev libvirglrenderer-dev meson python3-sphinx python3-sphinx-rtd-theme quilt xfslibs-dev
